f2df9a1bd05d6443ef36c1cb9c7fbfbaf5afd77b
Axel E Clemente Sosa <axeleclementesosa@Axels-MacBook-Air.local>
2025-07-08 16:12:44 +0200

    landing page redesing v12

--- app/[locale]/components/new-lading-page/footer.tsx
+++ app/[locale]/components/new-lading-page/footer.tsx
@@ -60,9 +60,9 @@ export default function Footer() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {/* Main Footer Content */}
         <div className="py-16">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8">
+          <div className="grid grid-cols-2 lg:grid-cols-6 gap-8">
             {/* Logo */}
-            <div className="lg:col-span-1">
+            <div className="col-span-2 lg:col-span-1 mb-4 lg:mb-0">
               <Link href="/" className="inline-block">
                 <div className="w-12 h-12 rounded-2xl overflow-hidden flex items-center justify-center bg-white">
                   <Image
